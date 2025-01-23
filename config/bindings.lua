44e94cbae55274d0c72e04c30c422533efba00f4
钱松 <2744588786@qq.com>
2025-01-23 19:14:47 +0800

    修复类Unix系统启动shell无法正常加载相关shell配置，导致PATH不生效的问题#12

--- config/bindings.lua
+++ config/bindings.lua
@@ -7,7 +7,7 @@ local mod = {}
 if platform.is_mac then
   mod.SUPER = "SUPER"
   mod.SUPER_REV = "SUPER|CTRL"
-elseif platform.is_win then
+elseif platform.is_win or platform.is_linux then
   mod.SUPER = "ALT" -- to not conflict with Windows key shortcuts
   mod.SUPER_REV = "ALT|CTRL"
 end
